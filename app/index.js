54e540297460aaf103022e93227fac4d3fe21bdb
Cami <39018077@ies9023.net>
2023-10-18 20:47:51 -0300

    quinto commit

--- app/index.js
+++ app/index.js
@@ -9,12 +9,14 @@ const app = express();
 app.set("port",4000);
 app.listen(app.get("port"));//escuchar el puerto
 console.log("Servidor corriendo en puerto",(app.get("port"))) //servidor para protger los datos, hacerlo en backend(adm sin darle acceso)
+
+//Configuracion//
+app.use(express.static(__dirname + "/public"));
+
 //Rutas: conectar con la pagina//
 app.get("/",(req,res)=> res.sendFile(__dirname +"/pages/login.html")) //que se gete la pagina, dando funcion de respuesta y consulta
 app.get("/register",(req,res)=> res.sendFile(__dirname +"/pages/register.html"))
 
-//Configuracion//
-app.use(express.static(__dirname + "/public"));
 
 
 
