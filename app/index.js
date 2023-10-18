b02bca33ca0116fb448a206519a6a2ef4379bfcd
Cami <39018077@ies9023.net>
2023-10-18 18:21:11 -0300

    Cuarto Commit

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
-console.log("Registrarse")
+import express from "express"
+//dirname:sino no funciona por el moduele
+import path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//servidor//
+const app = express();
+app.set("port",4000);
+app.listen(app.get("port"));//escuchar el puerto
+console.log("Servidor corriendo en puerto",(app.get("port"))) //servidor para protger los datos, hacerlo en backend(adm sin darle acceso)
+//Rutas: conectar con la pagina//
+app.get("/",(req,res)=> res.sendFile(__dirname +"/pages/login.html")) //que se gete la pagina, dando funcion de respuesta y consulta
+app.get("/register",(req,res)=> res.sendFile(__dirname +"/pages/register.html"))
+
+//Configuracion//
+app.use(express.static(__dirname + "/public"));
+
+
+
